39e7a26245701383a28e60138e8f120714b72e08
e-sparrow-life <e.sparrow.life@gmail.com>
2023-04-21 12:31:41 +0300

    Fixed sprite task

--- app/js/libs.js
+++ app/js/libs.js
@@ -1,3 +1,3 @@
 @if(0){@include_once('libs/_inputmask.js')}
-@if(0){@include_once('libs/_lazyload.js')}
+@if(1){@include_once('libs/_lazyload.js')}
 @if(0){@include_once('libs/_swiper-bundle.min.js')}
--- app/js/script.js
+++ app/js/script.js
@@ -3,7 +3,28 @@
 // https://github.com/verlok/vanilla-lazyload/#user-content-options
 // lazyLoadInstance.update(); - обновить 
 const lazyLoad = new LazyLoad();
- 
+
 
 // Инклудим файлы
 @include_once('modules/_burger.js')
+
+
+
+
+{
+    const defaultFontSize = 10;
+    const documentWidth = 1600;
+
+    function setRemSize() {
+        if (window.innerWidth < documentWidth && window.innerWidth > 1260) {
+            let value = (defaultFontSize / 100) * (window.innerWidth / (documentWidth / 100));
+            document.documentElement.style.fontSize = (value.toFixed(1) > defaultFontSize) ? defaultFontSize + 'px' : value.toFixed(1) + 'px';
+            console.log(value.toFixed(1))
+        } else {
+            document.documentElement.style.fontSize = defaultFontSize + 'px';
+        }
+        console.log(1)
+    }
+
+    window.addEventListener('resize', setRemSize);
+}
